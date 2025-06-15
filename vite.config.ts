716a1ee901a46aa1074dd11d3db0630186829caf
maverick <arunkumar.kaswan@highmarkhealth.org>
2025-06-15 16:37:19 -0400

    tree

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  base: '/village-library/', // this must match your repo name
+  plugins: [react()],
 })
