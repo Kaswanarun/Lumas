33122e873afd712c0c099572f66a4fbfadcb4862
maverick <arunkumar.kaswan@highmarkhealth.org>
2025-06-15 21:15:09 -0400

    sdf

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { HashRouter } from 'react-router-dom';  // 👈 Import HashRouter
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>  {/* 👈 Use HashRouter instead of BrowserRouter */}
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
